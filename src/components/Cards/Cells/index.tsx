b713dade255d7babc61a7219d318ddf77ea4f22b
SamuelFeijo19 <smuelfeijo@gmail.com>
2023-12-03 16:30:58 -0300

    feat: adicionar e remover user ao grupo

--- src/components/Cards/Cells/index.tsx
+++ src/components/Cards/Cells/index.tsx
@@ -1,11 +1,13 @@
 import {Modal} from "../../Modal";
-import {useState} from "react";
+import {useEffect, useState} from "react";
 import {Button} from "../../Button";
 import formatDate from "../../../utils";
 
 import {BeatLoader} from "react-spinners";
 
 import * as S from "./styles";
+import api from "../../../services/api.ts";
+import {toast} from "react-toastify";
 
 interface IEventCard {
     image: string | null;
@@ -19,6 +21,12 @@ interface IEventCard {
     onDelete: (id: string) => void;
     onEdit: (event: any) => void;
     loading: boolean;
+}
+
+interface Users {
+    id: string;
+    name: string;
+    email: string;
 }
 
 export const CellsCard = ({
@@ -35,11 +43,56 @@ export const CellsCard = ({
                           }: IEventCard) => {
     const [modalOpen, setModalOpen] = useState(false);
     const [deleteClicked, setDeleteClicked] = useState(false);
+    const [selectedUserId, setSelectedUserId] = useState<string | null>(null);
+    const [usersAll, setUsers] = useState<Users[]>([]);
 
     const handleDelete = () => {
         setDeleteClicked(true);
         onDelete(id);
         setModalOpen(false);
+    };
+
+    useEffect(() => {
+        const fetchUsers = async () => {
+            try {
+                const response = await api.get("/user");
+                setUsers(response.data.users);
+            } catch (error) {
+                console.error("Error fetching users:", error);
+            }
+        };
+
+        fetchUsers();
+    }, []);
+
+    const handleSubscribe = async (CellId: String) => {
+        try {
+            console.log(CellId)
+            if (selectedUserId) {
+                await api.put(`/group/add-member/${CellId}/${selectedUserId}`);
+
+                setSelectedUserId(null);
+
+                toast.success("Participante adicionado com sucesso");
+            } else {
+                toast.error("Participante não selecionado");
+            }
+        } catch (error) {
+            toast.error("Não foi possível adicionar participante" + error);
+        }
+    };
+
+    const handleRemoveMember = async (groupId: string) => {
+        try {
+            if(selectedUserId) {
+                await api.put(`/group/remove-member/${groupId}/${selectedUserId}`);
+                toast.success("Participante removido com sucesso");
+            }else {
+                toast.error("Participante não selecionado");
+            }
+        } catch (error) {
+            toast.error("Não foi possível remover o participante" + error);
+        }
     };
 
     return (
@@ -83,6 +136,31 @@ export const CellsCard = ({
                     <span>{address}</span>
                     <p>Criado em: {formatDate(createdAt)}</p>
                     <p>Atualizado em: {formatDate(updatedAt)}</p>
+                    <select
+                        name="selectedUserId"
+                        onChange={(e) => setSelectedUserId(e.target.value)}
+                        value={selectedUserId || ""}
+                        required
+                    >
+                        <option value="" disabled hidden>
+                            Escolha um participante
+                        </option>
+                        {usersAll?.map((user: Users) => (
+                            <option key={user.id} value={user.id}>
+                                {user.name} - {user.email}
+                            </option>
+                        ))}
+                    </select>
+                    <button type="button" onClick={() => handleSubscribe(String(id))}>
+                        Inscrever Participante
+                    </button>
+
+                    <button
+                        type="button"
+                        onClick={() => handleRemoveMember(String(id))}
+                    >
+                        Remover Participante
+                    </button>
                 </Modal>
             </S.CardEvent>
         </>
